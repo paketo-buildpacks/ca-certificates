e8ca9a02c0c36a78e7cf11ddcd7f3754783656fb
Ranganath Nadella <ranganath.nadella@excent.co.uk>
2023-05-10 14:05:11 +0100

    Updated_OpenSSL_URL

--- cacerts/certs.go
+++ cacerts/certs.go
@@ -34,7 +34,7 @@ import (
 )
 
 // Environment variables and defaults used by openssl to load trusted CA certificates
-// (see https://www.openssl.org/docs/man1.1.0/man3/SSL_CTX_set_default_verify_paths.html)
+// (see https://www.openssl.org/docs/manmaster/man3/SSL_CTX_set_default_verify_paths.html)
 const (
 	// EnvCAPath is the environment variable that can be used to set CApath
 	EnvCAPath string = "SSL_CERT_DIR"
